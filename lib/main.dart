825bfa274402c7e53ee7fbc02c6966e8677346a8
guilherme <tairentsuki@gmail.com>
2025-10-25 21:55:30 -0300

    Aula1

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(
+    Center(
+      child: Container(
+        alignment: Alignment.center,
+        child: Text(
+          "Hello World!",
+          style: TextStyle(color: Colors.yellow, fontSize: 45),
+          textDirection: TextDirection.ltr,
+        ),
+      ),
+    ),
+  );
+}
