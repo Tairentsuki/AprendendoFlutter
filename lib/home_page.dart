07638814b40015640ca91cea4de90f472963bdc9
guilherme <tairentsuki@gmail.com>
2025-10-25 23:04:43 -0300

    Aula4

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -12,19 +12,44 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: GestureDetector(
-        child: Text("Contador: $counter"),
-        onTap: () {
-          setState(() {
-            counter++;
-          });
-        },
-        onDoubleTap: () {
-          setState(() {
-            counter--;
-          });
-        },
+    return Scaffold(
+      appBar: AppBar(title: Text("Aula 5")),
+      body: Center(
+        child: GestureDetector(
+          child: Text("Contador: $counter", style: TextStyle(fontSize: 35,fontWeight: FontWeight.bold), ),
+          onTap: () {
+            setState(() {
+              counter++;
+            });
+          },
+          onDoubleTap: () {
+            setState(() {
+              counter--;
+            });
+          },
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        spacing: 10,
+        children: [
+          FloatingActionButton(
+            child: Icon(Icons.exposure_minus_1),
+            onPressed: () {
+              setState(() {
+                counter--;
+              });
+            },
+          ),
+          FloatingActionButton(
+            child: Icon(Icons.exposure_plus_1),
+            onPressed: () {
+              setState(() {
+                counter++;
+              });
+            },
+          ),
+        ],
       ),
     );
   }
